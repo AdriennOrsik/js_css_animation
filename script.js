81fde969ff84bb169f602649875b1d21909b0d40
Adrienn Orsik <5889371+AdriennOrsik@users.noreply.github.com>
2020-12-16 12:13:32 +0100

    Created animation files

---
+++ script.js
@@ -0,0 +1,36 @@
+function pageLoad() {
+
+let myObjects = [
+		{
+			tag: "h1",
+			content: "Orsik Adrienn"
+    },
+    {
+    tag: "p",
+    content: "Front End Developer"
+    }
+  ];
+
+  /* for(let i= 0; i < myObjects.length; i++) {
+
+  } */
+
+  const root = document.getElementById("root");
+
+  for(o of myObjects) {
+    console.log(o.tag);
+  }
+
+for(o of myObjects) {
+  root.insertAdjacentHTML('beforeend', `
+    <${o.tag}>${o.content}</${o.tag}>
+    `);
+}
+
+root.addEventListener('click', function() {
+root.classList.toggle('clicked');
+});
+
+}
+
+window.addEventListener('load', pageLoad);
